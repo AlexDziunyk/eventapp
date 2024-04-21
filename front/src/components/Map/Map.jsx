fccce3cfddc8667de07b4243fcbe4ca2e2289f2e
AlexDziunyk <alexdziunyk@gmail.com>
2024-04-21 22:14:26 +0300

    Added map to the app

--- front/src/components/Map/Map.jsx
+++ front/src/components/Map/Map.jsx
@@ -1,33 +1,32 @@
-import GoogleMapReact from 'google-map-react';
+import { APIProvider, Map as GoogleMap, AdvancedMarker } from '@vis.gl/react-google-maps';
 import marker from '../../static/img/marker.png';
 
 const Marker = () => <div>
   <img width={20} height={20} src={marker} alt='marker'></img>
 </div>;
 
-const Map = ({ lat, lng }) => {
-  const defaultProps = {
-    center: {
-      lat: 10.99835602,
-      lng: 77.01502627
-    },
-    zoom: 16
-  };
+const Map = () => {
+
+  const position = {
+    lat: 10.99835602,
+    lng: 77.01502627
+  }
 
   return (
-    <div style={{ height: '500px', width: '100%' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyCaHsRUO7Zrpc9UrisK-qkI0yyoLbt1lbk" }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
-      >
-        <Marker
-          lat={10.99835602}
-          lng={77.01502627}
-        />
-      </GoogleMapReact>
+    <div >
+      <APIProvider apiKey={"AIzaSyCaHsRUO7Zrpc9UrisK-qkI0yyoLbt1lbk"}>
+        <GoogleMap
+          style={{ width: '100%', height: '600px' }}
+          mapId={"8404e0bab090a21f"}
+          defaultCenter={position}
+          defaultZoom={16}
+          gestureHandling={'greedy'}
+          disableDefaultUI={true}>
+          <AdvancedMarker position={position}></AdvancedMarker>
+        </GoogleMap>
+      </APIProvider>
     </div>
   );
 }
 
-export default Map
+export default Map;
