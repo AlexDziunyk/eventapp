39e0b8f5878a1aa94e7505f4a0d89ec2d304cb79
AlexDziunyk <alexdziunyk@gmail.com>
2024-05-03 05:00:36 +0300

    Fixes to jwt

--- back/controllers/userController.js
+++ back/controllers/userController.js
@@ -85,7 +85,7 @@ const loginUser = async (req, res) => {
       return res.status(400).json({ message: 'Invalid credentials' });
     }
 
-    const token = jwt.sign({ userId: user._id, login: user.login }, 'your_secret_key', { expiresIn: '1h' });
+    const token = jwt.sign({ userId: user._id, login: user.login }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
     res.status(200).json({ message: 'Login successful', token });
   } catch (error) {
