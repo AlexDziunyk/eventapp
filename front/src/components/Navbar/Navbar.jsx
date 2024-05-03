2cf2e5e0d5a5be9ab1362ba505c8eebc8ffb6fe8
saantiisaa <ariakanzaki69@gmail.com>
2024-05-03 11:47:01 +0300

    fixed navbar

--- front/src/components/Navbar/Navbar.jsx
+++ front/src/components/Navbar/Navbar.jsx
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink, Link } from "react-router-dom";
 import './style.scss';
 
 const Navbar = () => {
-  return (
-    <div className='navbar'>
-      <Link to="/" className='logo'>uevents</Link>
-      <div className='navbar__panel'>
-        <NavLink to="/" className='navbar__item'>HOME</NavLink>
-        {/* <NavLink to="/formats" className='navbar__item'>FORMATS</NavLink>
-        <NavLink to="/themes" className='navbar__item'>THEMES</NavLink> */}
-        <NavLink to="/profile" className='navbar__item'>Profile</NavLink>
-        <NavLink to="/create" className='navbar__item'>Create Event</NavLink>
-      </div>
-      <div className='navbar__panel'>
-        <NavLink to="/login" className='navbar__login'>LOGIN</NavLink>
-        <NavLink to="/signup" className='navbar__signup'>SIGNUP</NavLink>
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-      </div>
-    </div>
-  )
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        setIsAuthenticated(!!token);
+    }, []);
+
+    return (
+        <div className='navbar'>
+            <Link to="/" className='logo'>uevents</Link>
+            <div className='navbar__panel'>
+                <NavLink to="/" className='navbar__item'>HOME</NavLink>
+                {isAuthenticated ? (
+                    <>
+                        {/* <NavLink to="/formats" className='navbar__item'>FORMATS</NavLink>
+            <NavLink to="/themes" className='navbar__item'>THEMES</NavLink> */}
+
+                        <NavLink to="/create" className='navbar__item'>Create Event</NavLink>
+                        <NavLink to="/profile" className='navbar__item'>Profile</NavLink>
+                    </>
+                ) : (
+                    <>
+                        <NavLink to="/login" className='navbar__login'>LOGIN</NavLink>
+                        <NavLink to="/signup" className='navbar__signup'>SIGNUP</NavLink>
+                    </>
+                )}
+            </div>
+        </div>
+    );
 }
 
-export default Navbar
+export default Navbar;
