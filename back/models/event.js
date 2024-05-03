fed3708c51a6bdc255c48afa9138d6586d229a5e
Fallen-Eleonor <Daniil.Dunai@cs.khpi.edu.ua>
2024-05-03 03:14:31 +0300

    fix company

--- back/models/event.js
+++ back/models/event.js
@@ -1,13 +1,57 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const companySchema = new Schema({
-  companyName: { type: String, required: true },
-  companyEmail: { type: String, required: true },
-  location: { type: String, required: true },
-  events: [{ type: Schema.Types.ObjectId, ref: 'Event' }],
-}, { timestamps: true });
+const eventSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true
+  },
+  lat: {
+    type: Number,
+    required: true
+  },
+  lng: {
+    type: Number,
+    required: true
+  },
+  placeName: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  format: {
+    type: String,
+    required: true,
+  },
+  theme: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: String,
+    required: true,
+  },
+  users: [{ type: Schema.Types.ObjectId, ref: 'user' }],
+  comments: [{ type: Schema.Types.ObjectId, ref: 'comment' }]
 
-const Company = mongoose.model('Company', companySchema);
+}, { timestamps: true })
 
-module.exports = Company;
+const Event = mongoose.model('event', eventSchema);
+
+module.exports = { Event, eventSchema };
+
